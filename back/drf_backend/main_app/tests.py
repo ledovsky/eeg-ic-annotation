08b4062c3576001e09d1fea4a52000b1b2de262b
Alexander Ledovsky <ledovskiy.a.dm@sberbank.ru>
2020-11-08 15:24:44 +0300

    Added 2 more plots

--- back/drf_backend/main_app/tests.py
+++ back/drf_backend/main_app/tests.py
@@ -2,13 +2,14 @@ from os.path import join
 
 import pandas as pd
 import numpy as np
+import matplotlib
 
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.apps import apps
 
-from .vis import plot_topomap
+from .vis import plot_topomap, plot_epochs_image, plot_spectrum
 from .models import Dataset, ICAComponent
 
 
@@ -43,9 +44,21 @@ class ICAViewTest(TestCase):
 
 class VisTest(TestCase):
     def setUp(self):
-        df = pd.read_csv(join(app_path, 'test_data/ica_weights.csv'))
-        self.ica_component = df['value'].values
-        self.ch_names = df['ch_name'].values
+        self.sfreq = 160.
+        df_weights = pd.read_csv(join(app_path, 'test_data/ica_weights.csv'))
+        df_data = pd.read_csv(join(app_path, 'test_data/ica_data.csv'))
+        self.ica_component = df_weights['value'].values
+        self.ch_names = df_weights['ch_name'].values
+        self.df_data = df_data
 
     def test_topomap(self):
-        plot_topomap(self.ica_component, self.ch_names)
+        fig = plot_topomap(self.ica_component, self.ch_names)
+        self.assertEqual(matplotlib.figure.Figure == type(fig))
+
+    def test_spectrum(self):
+        fig = plot_spectrum(self.df_data, self.sfreq)
+        self.assertEqual(matplotlib.figure.Figure == type(fig))
+
+    def test_epochs_image(self):
+        fig = plot_epochs_image(self.df_data)
+        self.assertEqual(matplotlib.figure.Figure == type(fig))
