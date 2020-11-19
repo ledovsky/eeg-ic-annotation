4e0544426b03f4d2b905c2794a2eac89b35efe85
Alexander Ledovsky <ledovskiy.a.dm@sberbank.ru>
2020-11-19 17:27:55 +0300

    Added recalc dataset command

---
+++ back/drf_backend/main_app/management/commands/recalc_dataset.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from main_app.models import ICAImages, ICALinks
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        # Named arguments
+        parser.add_argument(
+            '--dataset',
+            action='store',
+            required=True,
+            help='',
+        )
+
+    def handle(self, *args, **options):
+        dataset_short_name = options['dataset']
+        ICAImages.update_images(dataset_short_name)
+        ICALinks.update_links(dataset_short_name)
